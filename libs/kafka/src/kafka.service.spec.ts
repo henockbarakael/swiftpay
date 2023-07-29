ab4f5941f9fef3dab107ce41bedbec66ea409eaa
Nathan Sadala <bernynath@gmail.com>
2023-07-29 14:23:28 +0100

    % finish service

--- libs/kafka/src/kafka.service.spec.ts
+++ libs/kafka/src/kafka.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { WalletService } from './wallet.service';
+import { KafkaService } from './kafka.service';
 
-describe('WalletService', () => {
-  let service: WalletService;
+describe('KafkaService', () => {
+  let service: KafkaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalletService],
+      providers: [KafkaService],
     }).compile();
 
-    service = module.get<WalletService>(WalletService);
+    service = module.get<KafkaService>(KafkaService);
   });
 
   it('should be defined', () => {
