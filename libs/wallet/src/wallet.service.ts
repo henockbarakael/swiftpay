74838d9aa63f14aa0e20d7fcb7fa26736bb082ad
Nathan Sadala <bernynath@gmail.com>
2023-07-29 13:44:53 +0100

    add: refactor wallet service

--- libs/wallet/src/wallet.service.ts
+++ libs/wallet/src/wallet.service.ts
@@ -21,99 +21,7 @@ export class WalletService {
     return wallet;
   }
 
-  private isEnoughMoney(wallet: any, amount: number): boolean {
-    return wallet.balance > amount ? true : false;
-  }
-
-  async debitWallet(
-    currency: string,
-    merchantId: string,
-    amount: number,
-  ): Promise<any> {
-    const wallet = await this.getWalletbyCurrency(merchantId, currency);
-    const check = this.isEnoughMoney(wallet, amount);
-
-    if (check) {
-      const previousBalance = wallet.balance;
-      const actualBalance = wallet.balance - amount;
-      wallet.balance = actualBalance;
-
-      // update wallet
-      const update = await this.service.merchantWallet.update({
-        data: {
-          balance: wallet.balance,
-        },
-        where: {
-          id: wallet.id,
-        },
-      });
-
-      // add new record to history
-      const result = await this.service.merchantWalletHistory.create({
-        data: {
-          action: 'debit',
-          amount: amount,
-          previousBalance: previousBalance,
-          actualBalance: actualBalance,
-          merchantWalletId: wallet.id,
-        },
-      });
-
-      return { result: result, create: update };
-    } else {
-      return false;
-    }
-  }
-
-  async creditWallet(
-    currency: string,
-    merchantId: string,
-    amount: number,
-  ): Promise<any> {
-    const wallet = await this.getWalletbyCurrency(merchantId, currency);
-
-    const previousBalance = wallet.balance;
-    const actualBalance = wallet.balanc + amount;
-    wallet.balance = actualBalance;
-
-    // update wallet
-    const update = await this.service.merchantWallet.update({
-      data: {
-        balance: wallet.balance,
-      },
-      where: {
-        id: wallet.id,
-      },
-    });
-
-    // add new record to history
-    const create = await this.service.merchantWalletHistory.create({
-      data: {
-        action: 'debit',
-        amount: amount,
-        previousBalance: previousBalance,
-        actualBalance: actualBalance,
-        merchantWalletId: wallet.id,
-      },
-    });
-
-    return { update: update, create: create };
-  }
-
-  async refund(
-    amount: number,
-    merchantId: string,
-    currency: string,
-  ): Promise<any> {
-    const wallet = await this.service.merchantWallet.findUnique({
-      where: {
-        merchantId: merchantId,
-        currency: {
-          currency: currency,
-        },
-      },
-    });
-
+  private async performUpdate(wallet: any, amount: number): Promise<any> {
     const previousBalance = wallet.balance;
     const actualBalance = wallet.balance + amount;
     wallet.balance = actualBalance;
@@ -139,5 +47,51 @@ export class WalletService {
     });
 
     return { update: update, create: create };
+  }
+
+  private isEnoughMoney(wallet: any, amount: number): boolean {
+    return wallet.balance > amount ? true : false;
+  }
+
+  async debitWallet(
+    currency: string,
+    merchantId: string,
+    amount: number,
+  ): Promise<any> {
+    const wallet = await this.getWalletbyCurrency(merchantId, currency);
+    const check = this.isEnoughMoney(wallet, amount);
+
+    if (check) {
+      return await this.performUpdate(wallet, amount);
+    } else {
+      return false;
+    }
+  }
+
+  async creditWallet(
+    currency: string,
+    merchantId: string,
+    amount: number,
+  ): Promise<any> {
+    const wallet = await this.getWalletbyCurrency(merchantId, currency);
+
+    return await this.performUpdate(wallet, amount);
+  }
+
+  async refund(
+    amount: number,
+    merchantId: string,
+    currency: string,
+  ): Promise<any> {
+    const wallet = await this.service.merchantWallet.findUnique({
+      where: {
+        merchantId: merchantId,
+        currency: {
+          currency: currency,
+        },
+      },
+    });
+
+    return await this.performUpdate(wallet, amount);
   }
 }
