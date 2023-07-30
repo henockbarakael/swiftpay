1b9dc4cc08f838924689fc6698d2a53020e12ef6
Nathan Sadala <bernynath@gmail.com>
2023-07-30 09:38:50 +0100

    hybrid microservices

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client : {
+          brokers: process.env.KAFKA_BROKERS.split(',')
+        },
+        consumer : {
+          groupId: 'notification'
+        }
+      }
+    }
+  )
+  await app.listen();
 }
 bootstrap();
--- apps/telco-service/src/main.ts
+++ apps/telco-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { TelcoServiceModule } from './telco-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(TelcoServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    TelcoServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client : {
+          brokers: process.env.KAFKA_BROKERS.split(',')
+        },
+        consumer : {
+          groupId: 'telco'
+        }
+      }
+    }
+  )
+  await app.listen();
 }
 bootstrap();
