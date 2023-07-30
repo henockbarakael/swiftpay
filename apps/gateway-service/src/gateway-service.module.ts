e494f10a5d8f4c5825fc809ec8f50581c533918c
Nathan Sadala <bernynath@gmail.com>
2023-07-30 08:49:18 +0100

    feat: add kafka configuration to microservices

--- apps/gateway-service/src/gateway-service.module.ts
+++ apps/gateway-service/src/gateway-service.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { GatewayServiceController } from './gateway-service.controller';
 import { GatewayServiceService } from './gateway-service.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'gateway',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'gateway',
+            brokers: [
+              '161.35.161.18:9092',
+              '161.35.168.245:9092',
+              '161.35.165.31:9092',
+            ],
+          },
+          consumer: {
+            groupId: 'gateway',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [GatewayServiceController],
   providers: [GatewayServiceService],
 })
