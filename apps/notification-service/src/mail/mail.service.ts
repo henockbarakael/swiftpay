ce2736b8147d83a8c03f92d7e43d7663d713462a
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-08-03 04:44:46 +0100

    feat: add sendResetPasswordEmail method

--- apps/notification-service/src/mail/mail.service.ts
+++ apps/notification-service/src/mail/mail.service.ts
@@ -1,8 +1,37 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { i18nMails } from './internationalization';
 
 @Injectable()
 export class MailService {
-  sendResetPasswordEmail(data: string) {
-    throw new Error('Method not implemented.');
+  constructor(
+    private mailerService: MailerService,
+    private configService: ConfigService,
+  ) {}
+
+  async sendResetPasswordEmail(payload: string) {
+    const data = JSON.parse(payload);
+
+    const url = `${this.configService.get('BASE_URL')}/auth/reset-pwd/${
+      data.token
+    }`;
+
+    const text = i18nMails.resetPasswordTxt;
+    const subject = i18nMails.resetPasswordSubject;
+    const validation_text = i18nMails.confirm;
+
+    await this.mailerService.sendMail({
+      to: data.user.email,
+      subject: subject,
+      template: './confirmation',
+      context: {
+        name: `${data.user.firstName} ${data.user.lastName}`,
+        validation_url: url,
+        subject: subject,
+        text,
+        validation_text,
+      },
+    });
   }
 }
