a19c5984c3487e8337b6ab2667bd1ff7ea729699
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-09 08:23:07 +0100

    refactor: add Encryption module

---
+++ libs/encryption/src/encryption.module.ts
@@ -0,0 +1,11 @@
+import {EncryptionService} from "shared/encryption/encryption.service";
+import {ConfigModule} from "@nestjs/config";
+import {Global, Module} from "@nestjs/common";
+
+@Global()
+@Module({
+  providers: [EncryptionService],
+  exports: [EncryptionService],
+  imports: [ConfigModule.forRoot({})],
+})
+export class EncryptionModule {}
