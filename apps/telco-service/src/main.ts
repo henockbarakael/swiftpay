330267da9ca4ee3ef6ad65b3af09fb6a74397bc8
Nathan Sadala <bernynath@gmail.com>
2023-09-10 14:20:33 +0100

    changes

--- apps/telco-service/src/main.ts
+++ apps/telco-service/src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { TelcoServiceModule } from './telco-service.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(TelcoServiceModule);
+  const configService = app.get(ConfigService);
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
     options: {
       client: {
-        brokers: process.env.KAFKA_BROKERS.split(','),
+        brokers: configService.get('KAFKA_BROKERS').split(','),
       },
       consumer: {
         groupId: 'telco',
