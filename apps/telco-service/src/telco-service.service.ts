04920f387542ca9316ab89b113d9bb74feed33c3
Nathan Sadala <bernynath@gmail.com>
2023-09-30 00:32:15 +0100

    changes

--- apps/telco-service/src/telco-service.service.ts
+++ apps/telco-service/src/telco-service.service.ts
@@ -13,26 +13,29 @@ export class TelcoServiceService {
   async handleTelcoRequest(data: Request) {
     console.log(data);
     // request body
-    const body = {
-      merchant_id: `${process.env.merchant_id}`,
-      merchant_secret: `${process.env.merchant_secret}`,
-      amount: data.amount,
-      currency: `${data.currency}`,
-      action: `${data.action.toLowerCase()}`,
-      customer_number: `0${data.phoneNumber.slice(4)}`,
-      firstname: `${process.env.firstname}`,
-      lastname: `${process.env.lastname}`,
-      email: `${process.env.email}`,
-      reference: `${data.reference}`,
-      method:
-        data.service.toLowerCase() == 'vodacom'
-          ? 'mpesa'
-          : `${data.service.toLowerCase()}`,
-      callback_url: 'http://143.110.169.188:3700/',
-    };
+    try {
+      const body = {
+        merchant_id: `${process.env.merchant_id}`,
+        merchant_secret: `${process.env.merchant_secret}`,
+        amount: data.amount,
+        currency: `${data.currency}`,
+        action: `${data.action.toLowerCase()}`,
+        customer_number: `0${data.phoneNumber.slice(4)}`,
+        firstname: `${process.env.firstname}`,
+        lastname: `${process.env.lastname}`,
+        email: `${process.env.email}`,
+        reference: `${data.reference}`,
+        method:
+          data.service.toLowerCase() == 'vodacom'
+            ? 'mpesa'
+            : `${data.service.toLowerCase()}`,
+        callback_url: 'http://143.110.169.188:3700/',
+      };
 
-    const response = await axios.post(`${process.env.endpoint}`, body);
-
-    console.log(response.data);
+      const response = await axios.post(`${process.env.endpoint}`, body);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
