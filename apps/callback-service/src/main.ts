d14252cc4c18dc1a9144948cb23069097966da8e
Nathan Sadala <bernynath@gmail.com>
2023-10-02 21:15:05 +0100

    changes

--- apps/callback-service/src/main.ts
+++ apps/callback-service/src/main.ts
@@ -3,6 +3,7 @@ import { CallbackServiceModule } from './callback-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CallbackServiceModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3700);
 }
 bootstrap();
