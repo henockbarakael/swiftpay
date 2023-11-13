f5924841227aa906607e48fbda8f85dffe051ecb
rkota(Rosaire Kota) <rosaire@kadea.co>
2023-11-13 13:03:28 +0100

    refactor: add swagger ApiProperty

--- apps/gateway-service/src/dto/create-verification.dto.ts
+++ apps/gateway-service/src/dto/create-verification.dto.ts
@@ -1,63 +1,137 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ActionOperationEnum } from '@prisma/client';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty } from 'class-validator';
 import { normalizePhoneNumber } from 'libs/utils';
 export class CreateVerificationDto {
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   merchantID: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   @Transform(({ value }) => normalizePhoneNumber(value))
   phoneNumber: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   key: string;
 
+  
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   amount: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   currency: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   service: any;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   reference: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   action: ActionOperationEnum;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   callback_url: string;
 }
 
 export class CheckMarchantVerificationDto {
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   merchantID: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   @Transform(({ value }) => normalizePhoneNumber(value))
   phoneNumber: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   key: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   amount: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   currency: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   service: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   reference: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   action: ActionOperationEnum;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'This property is required',
+  })
   callback_url: string;
 }
